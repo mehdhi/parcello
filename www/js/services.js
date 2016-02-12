57f6fcf1cdf8e9d03f5db0666ee68702b4be4562
Mehdhi Nawaz <mehdhinawaz@gmail.com>
2016-02-13 00:48:32 +0530

    Added proper filler data

--- www/js/services.js
+++ www/js/services.js
@@ -5,30 +5,51 @@ angular.module('starter.services', [])
 
   // Some fake testing data
   var orders = [{
-    id: 0,
-    name: 'Ben Sparrow',
-    lastText: 'You on your way?',
-    face: 'img/ben.png'
+    id: 154564,
+    name: 'Samsung Galaxy Note 5 - Kapruka',
+    description: 'Samsung Galaxy Note 5 shipping from Canada',
+    face: 'img/dispatched.png',
+    status: 'dispatched',
+    deliveryDate: '20-02-2016',
+    deliveryAddress: 'No. 19 Lily Avenue, Colombo 06',
+    senderInfo: 'Kapruka Pvt Ltd, SL, 0115326565'
+    
   }, {
-    id: 1,
-    name: 'Max Lynx',
-    lastText: 'Hey, it\'s me',
-    face: 'img/max.png'
+   id: 155415,
+    name: 'SAMSUNG 500GB SSD PRO',
+    description: 'Amazon SAMSUNG Evo ProIII SSD 500GB',
+    face: 'img/dispatched.png',
+    status: 'dispatched',
+    deliveryDate: '26-02-2016',
+    deliveryAddress: 'No. 19 Lily Avenue, Colombo 06',
+    senderInfo: 'Kapruka Pvt Ltd, SL, 0115326565'
   }, {
-    id: 2,
-    name: 'Adam Bradleyson',
-    lastText: 'I should buy a boat',
-    face: 'img/adam.jpg'
+   id: 157895,
+    name: 'Screen Guard - Ali Express',
+    description: '6 inch screen guard',
+    face: 'img/dispatched.png',
+    status: 'dispatched',
+    deliveryDate: '22-02-2016',
+    deliveryAddress: 'No. 19 Lily Avenue, Colombo 06',
+    senderInfo: 'Kapruka Pvt Ltd, SL, 0115326565'
   }, {
-    id: 3,
-    name: 'Perry Governor',
-    lastText: 'Look at my mukluks!',
-    face: 'img/perry.png'
+    id: 154648,
+    name: 'Apple ipod nano',
+    description: 'Apple iPod Nano 6th Generation shipped from California',
+    face: 'img/delivered.png',
+    status: 'delivered',
+    deliveryDate: '02-02-2016',
+    deliveryAddress: 'No. 19 Lily Avenue, Colombo 06',
+    senderInfo: 'Kapruka Pvt Ltd, SL, 0115326565'
   }, {
-    id: 4,
-    name: 'Mike Harrington',
-    lastText: 'This is wicked good ice cream.',
-    face: 'img/mike.png'
+    id: 154554,
+    name: 'Galaxy Tab Case - Kapruka',
+    description: 'Samsung Galaxy Note 10 Case shipping from Hong Kong',
+    face: 'img/pending.png',
+    status: 'pending',
+    deliveryDate: '03-03-2016',
+    deliveryAddress: 'No. 19 Lily Avenue, Colombo 06',
+    senderInfo: 'Kapruka Pvt Ltd, SL, 0115326565'
   }];
 
   return {
